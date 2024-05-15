3c6d09ce8832ba22aa093d115d50c96c7afb1c56
gusgh075 <gusgh075@naver.com>
2024-05-15 13:59:05 +0900

    feat : 데이터베이스 테이블 정보

---
+++ app/src/main/java/com/example/travelmemory/database/RouteInfo.java
@@ -0,0 +1,13 @@
+package com.example.travelmemory.database;
+
+public class RouteInfo {
+    public static final String TABLE_NAME = "travel_route";
+    public static final String COLUMN_NAME_ID = "id";
+    public static final String COLUMN_NAME_TRAVEL_LIST = "travel_list";
+    public static final String COLUMN_NAME_ROUTE_ORDER = "route_order";
+    public static final String COLUMN_NAME_RATING = "rating";
+    public static final String COLUMN_NAME_REVIEW = "review";
+    public static final String COLUMN_NAME_PHOTO_PATH = "photo_path";
+    public static final String COLUMN_NAME_TRAVEL_COMPANION = "travel_companion";
+
+}
